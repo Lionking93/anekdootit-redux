af5d08ecadf44813a67c002b401465464ab29f26
Lionking93 <leo.kallonen@gmail.com>
2019-03-14 20:23:37 +0200

    osa 6 t6.3

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -23,6 +23,12 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
+  switch (action.type) {
+    case 'VOTE':
+      const anecdote = state.find(a => a.id === action.data.id)
+      return state.map(a => a.id !== action.data.id ? a : { ...anecdote, votes: anecdote.votes + 1 })
+  }
+
   return state
 }
 
